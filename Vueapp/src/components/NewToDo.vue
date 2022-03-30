2dd320b2cce8b6087616f499163b2e219c139b7c
Peter Wlodarczak <wlodarczak@gmail.com>
2022-03-30 14:59:15 +0200

    Added CRUD

--- Vueapp/src/components/NewToDo.vue
+++ Vueapp/src/components/NewToDo.vue
@@ -1,11 +1,7 @@
 <script>
 let id = 0;
 
-const filters = {
-  all: (todos) => todos,
-  active: (todos) => todos.filter((todo) => !todo.completed),
-  completed: (todos) => todos.filter((todo) => todo.completed)
-}
+const STORAGE_KEY = 'vue-todo'
 
 export default {
   data() {
@@ -13,8 +9,19 @@ export default {
       newTodo: "",
       hideCompleted: false,
       editedTodo: null,
-      todos: [],
+      /*todos: [],*/
+      todos: JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]'),
+      visibility: 'all'
     };
+  },
+
+  watch: {
+    todos: {
+      handler(todos) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+      },
+      deep: true
+    }
   },
   computed: {
     filteredTodos() {
@@ -22,13 +29,9 @@ export default {
         ? this.todos.filter((t) => !t.done)
         : this.todos;
     },
-    /*
-    hideactiveTodos() {
-      return this.hideActive
-        ? this.todos.filter((t) => t.done)
-        : this.todos;
-    },*/
-
+        remaining() {
+      return filters.active(this.todos).length
+    }
   },
   methods: {
     addTodo() {
@@ -39,8 +42,9 @@ export default {
       this.todos = this.todos.filter((t) => t !== todo);
     },
     editTodo(todo) {
-      this.beforeEditCache = this.todos.text
-      this.editedTodo = this.todos
+      console.log('=======>' + todo.text)
+      this.beforeEditCache = todo.text
+      this.editedTodo = todo
     },
     doneEdit(todo) {
       if (!this.editedTodo) {
@@ -51,14 +55,18 @@ export default {
       if (!todo.text) {
         this.removeTodo(todo)
       }
-    }
+    },
+    cancelEdit(todo) {
+      this.editedTodo = null
+      todo.text = this.beforeEditCache
+    },
   },
 };
 </script>
 
 <template>
-  <input v-model="newTodo" @keyup.enter="addTodo" />
-  <button @click="addTodo">Add Todo</button>
+  <input v-model="newTodo" @keyup.enter="addTodo" autofocus placeholder="What needs to be done?" />
+  <button @click="addTodo" >Add Todo</button>
   <br />
   <br />
   <table class="center">
@@ -66,10 +74,24 @@ export default {
       <th style="text-align: center; width: 300px">ToDo</th>
       <th>Remove</th>
     </tr>
-    <tr v-for="todo in filteredTodos" class="todo" :key="todo.id" :class="{ done: todo.done, editing: todo === editedTodo }">
+    <tr v-for="todo in filteredTodos" class="todo" :key="todo.id" :class="{ editing: todo === editedTodo }">
       <th style="width: 300px">
         <input type="checkbox" v-model="todo.done" />
         <label @dblclick="editTodo(todo)">{{ todo.text }}</label>
+      </th>
+      <th style="text-align: center">
+        <button @click="removeTodo(todo)">X</button>
+      </th>
+    </tr>
+  </table>
+  <br />
+      <ul class="todo-list">
+        <li
+          v-for="todo in filteredTodos"
+          class="todo"
+          :key="todo.id"
+          :class="{ editing: todo === editedTodo }"
+       >
         <input
             v-if="todo === editedTodo"
             class="edit"
@@ -79,59 +101,25 @@ export default {
             @blur="doneEdit(todo)"
             @keyup.enter="doneEdit(todo)"
             @keyup.escape="cancelEdit(todo)"
-          />
-        <!--span :class="{ done: todo.done }">{{ todo.text }}</span-->
-      </th>
-      <th style="text-align: center">
-        <button @click="removeTodo(todo)">X</button>
-      </th>
-    </tr>
-  </table>
+        />
+        </li>
+      </ul>
   <br />
   <button @click="hideCompleted = !hideCompleted">
     {{ hideCompleted ? "Show all" : "Hide completed" }}
   </button>
-
-        <ul class="todo-list">
-        <li
-          v-for="todo in filteredTodos"
-          class="todo"
-          :key="todo.id"
-          :class="{ done: todo.done, editing: todo === editedTodo }"
-        >
-          <div class="view">
-            <input class="toggle" type="checkbox" v-model="todo.done" />
-            <label @dblclick="editTodo(todo)">{{ todo.text }}</label>
-            <button class="destroy" @click="removeTodo(todo)"></button>
-          </div>
-          <input
-            v-if="todo === editedTodo"
-            class="edit"
-            type="text"
-            v-model="todo.text"
-            @vnode-mounted="({ el }) => el.focus()"
-            @blur="doneEdit(todo)"
-            @keyup.enter="doneEdit(todo)"
-            @keyup.escape="cancelEdit(todo)"
-          />
-        </li>
-      </ul>
-
-  <!--button @click="hideActive = !hideActive">
-    {{ hideCompleted ? 'Show all' : 'Hide active' }}
-  </button-->
-
   <p><button onclick="window.location.href='/index.html'">Home</button></p>
+
 </template>
 
 <style>
-@import "https://unpkg.com/todomvc-app-css@2.4.1/index.css";
+/*<@import "https://unpkg.com/todomvc-app-css@2.4.1/index.css";>*/
 .done {
   text-decoration: line-through;
 }
 body {
   text-align: center;
-  background-image: url("/wires.jpg");
+  background-image: url("/kacktus.jpg");
   background-size: cover;
 }
 table,
@@ -144,5 +132,10 @@ th {
 .center {
   margin-left: auto;
   margin-right: auto;
+}
+ul.todo-list {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
 }
 </style>
